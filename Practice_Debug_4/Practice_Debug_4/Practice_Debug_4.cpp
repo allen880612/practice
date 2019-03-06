7a448c87c581b950381fd412aadf1be9e460da22
allen880612 <allen880612@gmail.com>
2019-03-07 01:23:25 +0800

    Debug fininsh

---
+++ Practice_Debug_4/Practice_Debug_4/Practice_Debug_4.cpp
@@ -0,0 +1,64 @@
+#include "stdafx.h"
+#include <iostream> 
+using namespace std;
+void Swap(int num[], int indexA, int indexB)
+{
+	int tmp = num[indexA];
+	num[indexA] = num[indexB];
+	num[indexB] = tmp;
+}
+void BubbleSort(int num[], int length)
+{
+	for (int i = length - 1; i > 0; --i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			if (num[j] > num[j + 1])
+			{
+				Swap(num, j, j + 1);
+			}
+		}
+	}
+}
+void SelectionSort(int num[], int length)
+{
+	int i, j, max;
+	for (i = 0; i<length; i++)
+	{
+		max = i;
+		for (j = i + 1; j<length; j++)
+		{
+			if (num[j] > num[max])
+				max = j;
+		}
+		Swap(num, i, max);
+	}
+}
+
+int main(cli::array<System::String ^> ^args)
+{
+	int num[10] = { 12, 42, 23, 51, 8, 31, 24, 57, 78, 33 };
+	int length = 10; cout << "Array: "; for (int i = 0;
+		i<length; i++)
+	{
+		cout << num[i] << " ";
+	}
+	cout << endl; 
+
+	BubbleSort(num, length);	//Bubble Sort
+
+	cout << "Bubble Sort: ";
+	for (int i = 0; i<length; i++)
+	{
+		cout << num[i] << " ";
+	}
+	cout << endl;
+
+	
+	SelectionSort(num, length);	
+	cout << "Selection Sort: ";		//Selection Sort
+	for (int i = 0; i<length; i++)
+	{
+		cout << num[i] << " ";
+	}
+}
